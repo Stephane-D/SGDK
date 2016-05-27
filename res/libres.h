23b476dfac19965fcdac6ddbf6f7bb4e2af03ec7
Stephane Dallongeville <stephane.d@gmail.com>
2016-05-27 15:22:41 +0200

    changed resource compression

--- res/libres.h
+++ res/libres.h
@@ -1,9 +1,9 @@
-#ifndef _LIBRES_H_
-#define _LIBRES_H_
+#ifndef _D__APPS_SGDK_RES_LIBRES_H_
+#define _D__APPS_SGDK_RES_LIBRES_H_
 
 extern const Bitmap logo_lib;
 extern const TileSet font_lib;
 extern const Palette font_pal_lib;
 extern const u8 stop_xgm[172];
 
-#endif // _LIBRES_H_
+#endif // _D__APPS_SGDK_RES_LIBRES_H_
--- res/libres.res
+++ res/libres.res
@@ -1,4 +1,4 @@
-BITMAP logo_lib "image/logo_lib.png" 1
-TILESET font_lib "image/font_lib.png" 1
+BITMAP logo_lib "image/logo_lib.png" -1
+TILESET font_lib "image/font_lib.png" -1
 PALETTE font_pal_lib "image/font_lib.png"
 BIN stop_xgm "sound/stop_xgm.bin"
