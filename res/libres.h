1ff4e9268d7bfc640d59d99745fc5a163727b349
Stephane <stephane.d@outlook.com>
2017-06-19 20:53:33 +0200

    repair the mistake :)

---
+++ res/libres.h
@@ -0,0 +1,9 @@
+#ifndef _D__APPS_SGDK_RES_LIBRES_H_
+#define _D__APPS_SGDK_RES_LIBRES_H_
+
+extern const Bitmap logo_lib;
+extern const TileSet font_lib;
+extern const Palette font_pal_lib;
+extern const u8 stop_xgm[172];
+
+#endif // _D__APPS_SGDK_RES_LIBRES_H_
