0620f95123f34f4383eb35263e68f76bc62162e6
Stephane <stephane.d@outlook.com>
2018-09-11 12:26:18 +0200

    minors changes

--- sample/xgmplayer/res/music.h
+++ sample/xgmplayer/res/music.h
@@ -5,7 +5,7 @@ extern const u8 xgm_1[265102];
 extern const u8 xgm_2[128929];
 extern const u8 xgm_3[325901];
 extern const u8 xgm_4[112424];
-extern const u8 xgm_5[186023];
+extern const u8 xgm_5[186022];
 extern const u8 xgm_6[70335];
 extern const u8 xgm_7[29889];
 extern const u8 xgm_8[123929];
