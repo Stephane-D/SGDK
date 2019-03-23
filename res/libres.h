a0de6b1284910e1b6401ede31ea60df97ff299ad
Stephane <stephane.d@outlook.com>
2019-03-23 09:52:31 +0100

    Added new recomp tool (java version)

--- res/libres.h
+++ res/libres.h
@@ -1,9 +1,9 @@
-#ifndef _D__APPS_SGDK_RES_LIBRES_H_
-#define _D__APPS_SGDK_RES_LIBRES_H_
+#ifndef _RES_RES_LIBRES_H_
+#define _RES_RES_LIBRES_H_
 
-extern const Bitmap logo_lib;
-extern const TileSet font_lib;
-extern const Palette font_pal_lib;
 extern const u8 stop_xgm[172];
+extern const Palette font_pal_lib;
+extern const TileSet font_lib;
+extern const Bitmap logo_lib;
 
-#endif // _D__APPS_SGDK_RES_LIBRES_H_
+#endif // _RES_RES_LIBRES_H_
