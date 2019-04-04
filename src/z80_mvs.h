bcc3e3baebd593f779e21215720942958e108b9f
Stephane <stephane.d@outlook.com>
2019-04-04 14:57:32 +0200

    removed old / useless modules (TFM, MVS, VGM sound drivers, tile cache, zlib)
    - removed old TFM, MVS and VGM sound drivers (XGM sound driver is the sound driver to use).
    - removed old 'tile cache' unit (was deprecated and replaced by vram unit).
    - removed useless zlib unit (better to use APLIB or LZ4W compression).

--- src/z80_mvs.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Z80_MVS_H_
-#define _Z80_MVS_H_
-
-extern const u8 z80_mvs[0xA5B];
-
-#endif // _Z80_MVS_H_
--- src/z80_tfm.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Z80_TFM_H_
-#define _Z80_TFM_H_
-
-extern const u8 z80_tfm[0x69D];
-
-#endif // _Z80_TFM_H_
