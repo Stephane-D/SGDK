95be63c6bf956160f6794f653f04792d9b523bdd
Stephane <stephane.d@outlook.com>
2019-04-17 19:31:48 +0200

    fixed resources to be compatible with new rescomp tools

--- sample/xgmplayer/res/gfx.h
+++ sample/xgmplayer/res/gfx.h
@@ -1,12 +1,13 @@
 #ifndef _RES_GFX_H_
 #define _RES_GFX_H_
 
+extern const SpriteDefinition left_right;
+extern const SpriteDefinition cursor;
+extern const SpriteDefinition shadow_mask_16;
+extern const SpriteDefinition shadow_mask_7;
+extern const SpriteDefinition progress_bar;
 extern const Image bg;
 extern const Image starfield;
 extern const Image music_logo;
-extern const SpriteDefinition left_right;
-extern const SpriteDefinition cursor;
-extern const SpriteDefinition shadow_mask;
-extern const SpriteDefinition progress_bar;
 
 #endif // _RES_GFX_H_
