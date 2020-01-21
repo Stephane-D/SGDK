a23e94ac5573b65be573d34d3265ed230de0d87a
Stephane <stephane.d@outlook.com>
2020-01-21 17:52:32 +0100

    Added compression and far parameter to BIN resource

--- tools/rescomp/src/sgdk/rescomp/processor/BinProcessor.java
+++ tools/rescomp/src/sgdk/rescomp/processor/BinProcessor.java
@@ -4,6 +4,8 @@ import sgdk.rescomp.Compiler;
 import sgdk.rescomp.Processor;
 import sgdk.rescomp.Resource;
 import sgdk.rescomp.resource.Bin;
+import sgdk.rescomp.tool.Util;
+import sgdk.rescomp.type.Basics.Compression;
 import sgdk.tool.FileUtil;
 import sgdk.tool.StringUtil;
 
@@ -21,33 +23,48 @@ public class BinProcessor implements Processor
         if (fields.length < 3)
         {
             System.out.println("Wrong BIN definition");
-            System.out.println("BIN name file [align [salign [fill]]]");
-            System.out.println("  name\t\tBIN data variable name");
-            System.out.println("  file\tpath of the data file to convert to binary data array");
-            System.out.println("  align\tmemory address alignment for generated data array (default is 2)");
-            System.out.println("  salign\tsize alignment for the generated data array (default is 2)");
-            System.out.println("  fill\tfill value for the size alignment (default is 0)");
+            System.out.println("BIN name file [align [salign [fill [far]]]]");
+            System.out.println("  name          BIN data variable name");
+            System.out.println("  file          path of the data file to convert to binary data array");
+            System.out.println("  align         memory address alignment for generated data array (default is 2)");
+            System.out.println("  salign        size alignment for the generated data array (default is 2)");
+            System.out.println("  fill          fill value for the size alignment (default is 0)");
+            System.out.println("  compression   compression type, accepted values:");
+            System.out.println("                 -1 / BEST / AUTO = use best compression");
+            System.out.println("                  0 / NONE        = no compression (default)");
+            System.out.println("                  1 / APLIB       = aplib library (good compression ratio but slow)");
+            System.out.println(
+                    "                  2 / FAST / LZ4W = custom lz4 compression (average compression ratio but fast)");
+            System.out.println(
+                    "  far           'far' binary data flag to put it at the end of the ROM (useful for bank switch, default = TRUE)");
 
             return null;
         }
-
-        int align = 2;
-        int salign = 2;
-        int fill = 0;
 
         // get resource id
         final String id = fields[1];
         // get input file
         final String fileIn = FileUtil.adjustPath(Compiler.resDir, fields[2]);
         // get address alignment
+        int align = 2;
         if (fields.length >= 4)
             align = StringUtil.parseInt(fields[3], align);
         // get size alignment
+        int salign = 2;
         if (fields.length >= 5)
             salign = StringUtil.parseInt(fields[4], salign);
         // get fill value
+        int fill = 0;
         if (fields.length >= 6)
             fill = StringUtil.parseInt(fields[5], fill);
+        // get compression value
+        Compression compression = Compression.NONE;
+        if (fields.length >= 7)
+            compression = Util.getCompression(fields[6]);
+        // get far value
+        boolean far = true;
+        if (fields.length >= 8)
+            far = StringUtil.parseBoolean(fields[7], far);
 
         // read data from BIN file
         final byte[] data = FileUtil.load(fileIn, true);
@@ -55,21 +72,6 @@ public class BinProcessor implements Processor
         if (data == null)
             return null;
 
-        return new Bin(id, data, align, salign, fill);
-
-        // EXPORT BIN
-        // outBIN(data, align, outS, outH, id, true);
-        //
-        // return true;
+        return new Bin(id, data, align, salign, fill, compression, far);
     }
-
-    // public static void outBIN(byte[] data, int align, PrintWriter outS, PrintWriter outH, String
-    // id, boolean global)
-    // {
-    // // declare
-    // Util.declArray(outS, outH, "u8", id, data.length, align, true);
-    // // output data
-    // Util.outS(data, outS, 1);
-    // outS.println();
-    // }
 }
