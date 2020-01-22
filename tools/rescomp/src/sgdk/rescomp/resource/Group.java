8a6be2cefd2616668c9ee3faecdc1bc6667fec93
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-01-22 01:08:22 +0100

    added GROUP function

---
+++ tools/rescomp/src/sgdk/rescomp/resource/Group.java
@@ -0,0 +1,43 @@
+package sgdk.rescomp.resource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import sgdk.rescomp.Resource;
+
+public class Group extends Resource
+{
+    final int hc;
+
+    public Group(String id)
+    {
+        super(id);
+
+        // compute hash code
+        hc = 1;
+    }
+
+    @Override
+    public int internalHashCode()
+    {
+        return hc;
+    }
+
+    @Override
+    public boolean internalEquals(Object obj)
+    {
+        return (obj instanceof Group);
+    }
+
+    @Override
+    public int shallowSize()
+    {
+        return 0;
+    }
+
+    @Override
+    public void out(ByteArrayOutputStream outB, StringBuilder outS, StringBuilder outH) throws IOException
+    {
+        // nothing here
+    }
+}
