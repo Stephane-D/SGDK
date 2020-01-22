71502f5d786b74fc67dcf8d29733f87dcfc5526f
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-01-22 01:08:06 +0100

    made alignment parameter mandatory

--- tools/rescomp/src/sgdk/rescomp/processor/AlignProcessor.java
+++ tools/rescomp/src/sgdk/rescomp/processor/AlignProcessor.java
@@ -18,17 +18,20 @@ public class AlignProcessor implements Processor
     @Override
     public Resource execute(String[] fields) throws IOException
     {
-        if (fields.length < 2)
+        if (fields.length < 1)
         {
             System.out.println("Wrong ALIGN definition");
-            System.out.println("ALIGN alignment");
-            System.out.println("  alignment     specifies the minimum binary data alignment in bytes");
+            System.out.println("ALIGN [alignment]");
+            System.out.println(
+                    "  alignment     specifies the minimum binary data alignment in bytes (default is 524288)");
 
             return null;
         }
 
         // get alignment
-        final int alignment = StringUtil.parseInt(fields[1], 0);
+        int alignment = 524288;
+        if (fields.length > 1)
+            StringUtil.parseInt(fields[1], alignment);
 
         // build ALIGN resource
         return new Align("align", alignment);
