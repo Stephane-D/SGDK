96bed121ca7df94dbd68a3f4a7bcb7783f6b15c3
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-03-11 23:29:36 +0100

    Added ror / rol functions which are correctly turned into ROR / ROL instruction when optimization are enabled

--- inc/types.h
+++ inc/types.h
@@ -230,5 +230,66 @@ u8  getZeroU8();
 u16 getZeroU16();
 u32 getZeroU32();
 
+/**
+ *  \brief
+ *      ROL instruction for byte (8 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u8  rol8(u8 value, u16 number);
+/**
+ *  \brief
+ *      ROL instruction for short (16 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u16 rol16(u16 value, u16 number);
+/**
+ *  \brief
+ *      ROL instruction for long (32 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u32 rol32(u32 value, u16 number);
+/**
+ *  \brief
+ *      ROR instruction for byte (8 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u8  ror8(u8 value, u16 number);
+/**
+ *  \brief
+ *      ROR instruction for short (16 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u16 ror16(u16 value, u16 number);
+/**
+ *  \brief
+ *      ROR instruction for long (32 bit) value
+ *
+ *  \param value
+ *      value to apply bit rotation
+ *  \param number
+ *      number of bit rotation
+ */
+u32 ror32(u32 value, u16 number);
+
 
 #endif // _TYPES_H_
--- src/types.c
+++ src/types.c
@@ -16,3 +16,32 @@ u32 getZeroU32()
     return 0;
 }
 
+u8 rol8(u8 value, u16 number)
+{
+   return (value << number) | (value >> (8 - number));
+}
+
+u16 rol16(u16 value, u16 number)
+{
+   return (value << number) | (value >> (16 - number));
+}
+
+u32 rol32(u32 value, u16 number)
+{
+   return (value << number) | (value >> (32 - number));
+}
+
+u8 ror8(u8 value, u16 number)
+{
+   return (value >> number) | (value << (8 - number));
+}
+
+u16 ror16(u16 value, u16 number)
+{
+   return (value >> number) | (value << (16 - number));
+}
+
+u32 ror32(u32 value, u16 number)
+{
+   return (value >> number) | (value << (32 - number));
+}
