8a76b8e206872dd355f20efd6728fbf73a8dcb3d
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-03-22 21:33:47 +0100

    cleanup for deprecation

--- inc/vdp_dma.h
+++ inc/vdp_dma.h
@@ -30,46 +30,16 @@
 
 
 /**
- *  \brief
- *      Start DMA transfert to VRAM.
- *
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address in VRAM.
- *  \param len
- *      Number of word to transfert.
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 #define VDP_doVRamDMA(from, to, len)            \
     VDP_doDMA(VDP_DMA_VRAM, from, to, len)
 /**
- *  \brief
- *      Start DMA transfert to CRAM.
- *
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address in CRAM.
- *  \param len
- *      Number of word to transfert.
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 #define VDP_doCRamDMA(from, to, len)            \
     VDP_doDMA(VDP_DMA_CRAM, from, to, len)
 /**
- *  \brief
- *      Start DMA transfert to VSRAM.
- *
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address in VSRAM.
- *  \param len
- *      Number of word to transfert.
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 #define VDP_doVSRamDMA(from, to, len)           \
@@ -77,75 +47,18 @@
 
 
 /**
- *  \brief
- *      Do DMA transfer operation.
- *
- *  \param location
- *      Destination location.<br>
- *      Accepted values:<br>
- *      - VDP_DMA_VRAM (for VRAM transfert).<br>
- *      - VDP_DMA_CRAM (for CRAM transfert).<br>
- *      - VDP_DMA_VSRAM (for VSRAM transfert).<br>
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address.
- *  \param len
- *      Number of word to transfer.
- *  \param vramStep
- *      VRam address increment value (-1 to 255).<br>
- *      By default you should set it to 2 for normal copy operation but you can use different value
- *      for specific operation.<br>
- *      -1 means the VRam address increment register won't be modified (use current value).
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 void VDP_doDMAEx(u8 location, u32 from, u16 to, u16 len, s16 vramStep);
 /**
- *  \brief
- *      Do DMA transfert operation.
- *
- *  \param location
- *      Destination location.<br>
- *      Accepted values:<br>
- *      - VDP_DMA_VRAM (for VRAM transfert).<br>
- *      - VDP_DMA_CRAM (for CRAM transfert).<br>
- *      - VDP_DMA_VSRAM (for VSRAM transfert).<br>
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address.
- *  \param len
- *      Number of word to transfert.
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 void VDP_doDMA(u8 location, u32 from, u16 to, u16 len);
 /**
- *  \brief
- *      Do VRAM DMA fill operation.
- *
- *  \param to
- *      Destination address.
- *  \param len
- *      Number of byte to fill.
- *  \param value
- *      Fill value (byte).
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 void VDP_doVRamDMAFill(u16 to, u16 len, u8 value);
 /**
- *  \brief
- *      Do VRAM DMA copy operation.
-
- *  \param from
- *      Source address.
- *  \param to
- *      Destination address.
- *  \param len
- *      Number of byte to copy.
- *
  *  \deprecated Use DMA_xxx methods instead
  */
 void VDP_doVRamDMACopy(u16 from, u16 to, u16 len);
