a5c482b4929469a6f3ca2912ecfede2911a1abf0
Stephane <stephane.d@outlook.com>
2020-04-11 00:38:49 +0200

    Added memory information on intro screen

--- sample/bench/src/main.c
+++ sample/bench/src/main.c
@@ -3,10 +3,14 @@
 #include "gfx.h"
 
 
-#define SGDK_BENCHMARK      "SGDK benchmark v1.24"
+#define SGDK_BENCHMARK      "SGDK benchmark v1.3"
 
 #define MAX_TEST            9
 #define MAX_SUBTEST         16
+
+
+// end of bss
+extern u32 _bend;
 
 
 u16 detailledScores[MAX_TEST][MAX_SUBTEST];
@@ -117,7 +121,7 @@ int main()
         // fade text
         VDP_fadeOut(15, 15, 30, FALSE);
         // clear text
-        VDP_clearPlan(BG_A, TRUE);
+        VDP_clearPlane(BG_A, TRUE);
 
         // reset text color to white
         VDP_setPaletteColor(15, 0xEEE);
@@ -127,14 +131,32 @@ int main()
 
 static void showIntroScreen()
 {
+    char str[64];
+
     u16 col = 0xEEE;
 
     // set text color to black
     VDP_setPaletteColor(15, 0x000);
 
+    VDP_drawText("Total memory          65536 bytes", 0, 1);
+    sprintf(str, "Free memory           %05u bytes", MEM_getFree());
+    VDP_drawText(str, 0, 2);
+    sprintf(str, "Used memory           %05u bytes", 65536 - MEM_getFree());
+    VDP_drawText(str, 0, 3);
+    sprintf(str, "  Static allocation   %05u bytes", (u16) ((u32)&_bend) & 0xFFFF);
+    VDP_drawText(str, 0, 4);
+    sprintf(str, "  Stack size          %05u bytes", (u16) STACK_SIZE);
+    VDP_drawText(str, 0, 5);
+    sprintf(str, "  DMA queue & buffer  %05u bytes", DMA_getBufferSize() + (u16) (DMA_getMaxQueueSize() * sizeof(DMAOpInfo)));
+    VDP_drawText(str, 0, 6);
+    sprintf(str, "  Memory manager      %05u bytes", (65536 - MEM_getFree()) - (((u16) ((u32)&_bend) & 0xFFFF) + STACK_SIZE + DMA_getBufferSize() + (u16) (DMA_getMaxQueueSize() * sizeof(DMAOpInfo))));
+    VDP_drawText(str, 0, 7);
+    sprintf(str, "Free VRAM              %04d tiles", TILE_USERMAXINDEX);
+    VDP_drawText(str, 0, 9);
+
     // display test string
-    VDP_drawText(SGDK_BENCHMARK, 10, 10);
-    VDP_drawText("Press START to begin", 10, 14);
+    VDP_drawText(SGDK_BENCHMARK, 10, 14);
+    VDP_drawText("Press START to begin", 10, 16);
 
     // fade text color to white
     VDP_fadeIn(15, 15, &col, 30, FALSE);
@@ -143,7 +165,7 @@ static void showIntroScreen()
     // fade text
     VDP_fadeOut(15, 15, 30, FALSE);
     // clear text
-    VDP_clearPlan(BG_A, TRUE);
+    VDP_clearPlane(BG_A, TRUE);
 
     // reset text color to white
     VDP_setPaletteColor(15, 0xEEE);
@@ -169,7 +191,7 @@ static void preTest(char *title, s16 num)
     // fade text
     VDP_fadeOut(15, 15, 30, FALSE);
     // clear text
-    VDP_clearPlan(BG_A, TRUE);
+    VDP_clearPlane(BG_A, TRUE);
 
     // reset text color to white
     VDP_setPaletteColor(15, 0xEEE);
@@ -197,7 +219,7 @@ static void postTest(char *title, u16 score, s16 num)
     // fade text
     VDP_fadeOut(15, 15, 30, FALSE);
     // clear text
-    VDP_clearPlan(BG_A, TRUE);
+    VDP_clearPlane(BG_A, TRUE);
 
     // reset text color to white
     VDP_setPaletteColor(15, 0xEEE);
