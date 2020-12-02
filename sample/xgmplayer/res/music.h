c01fdc08e4c1aaed360badc8bf6015d683bdceba
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-12-03 00:10:05 +0100

    changed test track

--- sample/xgmplayer/res/music.h
+++ sample/xgmplayer/res/music.h
@@ -1,7 +1,7 @@
 #ifndef _RES_MUSIC_H_
 #define _RES_MUSIC_H_
 
-extern const u8 xgm_7[30208];
+extern const u8 xgm_6[70656];
 extern const u8 pcm_hat2[5632];
 extern const u8 pcm_voice[17408];
 extern const u8 pcm_snare2[3840];
