046a856b631ff712f1c90f4c10fd1e72e842e10e
Stephane <stephane.d@outlook.com>
2021-11-15 11:46:10 +0100

    Fixed unallowed sun 'WaveFileReader' class usage for WAV file resampling operation

--- tools/commons/src/sgdk/tool/SoundUtil.java
+++ tools/commons/src/sgdk/tool/SoundUtil.java
@@ -10,31 +10,43 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import com.sun.media.sound.WaveFileReader;
-
 public class SoundUtil
 {
     public static byte[] getRawDataFromWAV(String wavPath, int bits, int rate, boolean mono, boolean signed,
             boolean bigEndian) throws UnsupportedAudioFileException, IOException
     {
-        final File wavFile = new File(wavPath);
-        final WaveFileReader waveReader = new WaveFileReader();
-        final AudioInputStream wavInput = waveReader.getAudioInputStream(wavFile);
-
-        // get resampled input stream
-        final AudioInputStream resampledInput = resample(wavInput, bits, rate, mono, signed, bigEndian);
         // we will never need more than 128MB sample :p
         final byte[] result = new byte[128 * 1024 * 1024];
-
-        int read;
         int off = 0;
-        do
+
+        final File wavFile = new File(wavPath);
+        final AudioInputStream wavInput = AudioSystem.getAudioInputStream(wavFile);
+        // get resampled input stream
+        final AudioInputStream resampledInput = resample(wavInput, bits, rate, mono, signed, bigEndian);
+        
+        // cannot convert ?
+        if (resampledInput == null)
         {
-            read = resampledInput.read(result, off, 256);
-            if (read > 0)
-                off += read;
+            wavInput.close();
+            throw new UnsupportedAudioFileException("Error: couldn't resample '"+ wavPath + "' WAV file.");
         }
-        while (read > 0);
+
+        try
+        {
+            int read;
+            do
+            {
+                read = resampledInput.read(result, off, 256);
+                if (read > 0)
+                    off += read;
+            }
+            while (read > 0);
+        }
+        finally
+        {
+            resampledInput.close();
+            wavInput.close();
+        }
 
         return Arrays.copyOf(result, off);
     }
@@ -47,8 +59,8 @@ public class SoundUtil
         final int adjRate = ((rate <= 0) ? (int) audioInputFormat.getSampleRate() : rate);
 
         // define target format
-        final AudioFormat dstFormat = new AudioFormat(signed ? Encoding.PCM_SIGNED : Encoding.PCM_UNSIGNED, adjRate, bits,
-                channel, ((bits + 7) / 8) * channel, adjRate, bigEndian);
+        final AudioFormat dstFormat = new AudioFormat(signed ? Encoding.PCM_SIGNED : Encoding.PCM_UNSIGNED, adjRate,
+                bits, channel, ((bits + 7) / 8) * channel, adjRate, bigEndian);
 
         if (!AudioSystem.isConversionSupported(dstFormat, audioInputFormat))
             return null;
--- tools/rescomp/src/sgdk/rescomp/Launcher.java
+++ tools/rescomp/src/sgdk/rescomp/Launcher.java
@@ -30,7 +30,7 @@ public class Launcher
                 depTarget = param;
         }
 
-        System.out.println("ResComp 3.37 - SGDK Resource Compiler - Copyright 2021 (Stephane Dallongeville)");
+        System.out.println("ResComp 3.38 - SGDK Resource Compiler - Copyright 2021 (Stephane Dallongeville)");
 
         if (fileName == null)
         {
