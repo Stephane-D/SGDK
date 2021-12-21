ed72a4cb5ed16a4f4311ff337d3e09f44a44ac8b
doragasu <doragasu@hotmail.com>
2021-12-21 21:21:28 +0100

    Modifications to support Linux native toolchains.
    * Define SGDK_DOCKER=y in Docker image to allow detecting it.
    * Move common Makefile definitions to common.mk.
    * Add variables for Linux native configuration.

---
+++ common.mk
@@ -0,0 +1,67 @@
+# Common definitions
+
+BIN := $(GDK)/bin
+LIB := $(GDK)/lib
+
+SRC_LIB := $(GDK)/src
+RES_LIB := $(GDK)/res
+INCLUDE_LIB := $(GDK)/inc
+MAKEFILE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+MAKEFILE_DIR := $(subst \,/,$(MAKEFILE_DIR))
+
+ifeq ($(OS),Windows_NT)
+	# Native Windows
+	SHELL := $(BIN)/sh.exe
+	RM := $(BIN)/rm.exe
+	CP := $(BIN)/cp.exe
+	MKDIR := $(BIN)/mkdir.exe
+
+	AR := $(BIN)/ar.exe
+	CC := $(BIN)/gcc.exe
+	LD:= $(BIN)/ld.exe
+	NM:= $(BIN)/nm.exe
+	OBJCPY := $(BIN)/objcopy.exe
+	ASMZ80 := $(BIN)/sjasm.exe
+	MACCER := $(BIN)/mac68k.exe
+	BINTOS := $(BIN)/bintos.exe
+	LTO_PLUGIN := --plugin=liblto_plugin-0.dll 
+else
+	ifeq ($(SGDK_DOCKER),y)
+		# Linux docker
+		SHELL = sh
+		RM = rm
+		CP = cp
+		MKDIR = mkdir
+
+		AR := $(BIN)/ar
+		CC := $(BIN)/gcc
+		LD := $(BIN)/ld
+		NM := $(BIN)/nm
+		OBJCPY := $(BIN)/objcopy
+		ASMZ80 := $(BIN)/sjasm
+		MACCER := $(BIN)/mac68k
+		BINTOS := $(BIN)/bintos
+		LTO_PLUGIN := --plugin=liblto_plugin-0.dll
+	else
+		# Native Linux
+		PREFIX := m68k-elf-
+		SHELL = sh
+		RM = rm
+		CP = cp
+		MKDIR = mkdir
+
+		AR := $(PREFIX)ar
+		CC := $(PREFIX)gcc
+		LD := $(PREFIX)ld
+		NM := $(PREFIX)nm
+		OBJCPY := $(PREFIX)objcopy
+		ASMZ80 := sjasm
+		MACCER := mac68k
+		BINTOS := bintos
+	endif
+endif
+
+JAVA := java
+ECHO := echo
+SIZEBND := $(JAVA) -jar $(BIN)/sizebnd.jar
+RESCOMP := $(JAVA) -jar $(BIN)/rescomp.jar
