b234dc63b3b09afc4ef7d952e651247660528d90
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2022-03-03 17:30:03 +0100

    Added new base object unit (still very wip)

---
+++ inc/object.h
@@ -0,0 +1,157 @@
+/**
+ *  \file object.h
+ *  \brief Base object management unit.
+ *  \author Stephane Dallongeville
+ *  \date 02/2022
+ *
+ * This unit provides methods to manage objects.<br>
+ * It works in concert with the <i>pool.h</i> unit which provide dynamic object allocation.<br>
+ * <br>
+ * The idea of <i>Object</i> is that you can use it as base structure for your own object (entity, enemy, character, whatever you want..).<br>
+ * To do that the idea is to declare your new object structure by embedding the Object into it and always at the first position:<pre>
+ * struct entity_
+ * {
+ *     Object obj;
+ *     f32 posX;
+ *     f32 posY;
+ *     Sprite* sprite;
+ *     ...
+ * };</pre>
+ *
+ * Doing that your Entity structure can be used through OBJ_xxx methods for will be acceptedYou can use <i>Pool</i> object to handle dynamic allocation from a fixed set of objects.<br>
+ * For instance if you may need to handle dynamically bullets for your game and you want to have
+ * at max 20 bullets, you can handle it that way:<br><pre>
+
+ */
+
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include "pool.h"
+
+
+/**
+ * \brief Allocated state for object (mainly useful for debugging)
+ */
+#define OBJ_ALLOCATED       0x8000
+
+
+
+// forward
+typedef struct Object_ Object;
+
+/**
+ *  \brief
+ *      Object function callback
+ *
+ *  \param obj
+ *      Object
+ */
+typedef void ObjectCallback(Object* obj);
+
+/**
+ *  \brief
+ *      Base object structure
+ *
+ *  \param state
+ *      Object state
+ *  \param init
+ *      Initialisation function callback, called only once after object creation
+ *  \param update
+ *      Update function callback, usually called once per framecalled during
+ *  \param end
+ *      Ending function callback, called only once before object release
+ */
+typedef struct Object_
+{
+    u16 state;
+    ObjectCallback* init;
+    ObjectCallback* update;
+    ObjectCallback* end;
+} Object;
+
+
+/**
+ *  \brief
+ *      Create and allocate the new object pool (this method is an alias of #POOL_create(..))
+ *
+ *  \param size
+ *      the capacity of the pool (in number of object)
+ *  \param objectSize
+ *      the size of a single object (usually you should use sizeof(Struct) here)
+ *
+ *  \return the new created object pool or NULL if there is not enough memory available for that.
+ *
+ *  \see POOL_create(..)
+ */
+Pool* OBJ_createObjectPool(u16 size, u16 objectSize);
+
+/**
+ *  \brief
+ *      Create a new objet from the given object pool
+ *
+ *  \param pool
+ *      Object pool to allocate from (see pool.h unit)
+ *
+ *  \return the created object or NULL if an error occured (no more available object in pool or invalid pool).
+ *  The returned object is initialized to 0
+ *
+ *  \see OBJ_release(..)
+ */
+Object* OBJ_create(Pool* pool);
+/**
+ *  \brief
+ *      Release an objet from the given object pool
+ *
+ *  \param pool
+ *      Object pool allocator to release from (see pool.h unit)
+ *  \param obj
+ *      Object to release
+ *
+ *  \see OBJ_create(..)
+ */
+void OBJ_release(Pool* pool, Object* obj);
+
+/**
+ *  \brief
+ *      Set the initialization method for the given object
+ *
+ *  \param obj
+ *      Object to set <i>init</i> method for
+ *  \param initMethod
+ *      the method to set for object initialization (should be called after object creation)
+ *
+ *  \see OBJ_setUpdateMethod(..)
+ *  \see OBJ_setEndMethod(..)
+ */
+void OBJ_setInitMethod(Object* obj, ObjectCallback* initMethod);
+/**
+ *  \brief
+ *      Set the update method for the given object
+ *
+ *  \param obj
+ *      Object to set <i>update</i> method for
+ *  \param initMethod
+ *      the method to set for object update (should be called once per frame)
+ *
+ *  \see OBJ_setInitMethod(..)
+ *  \see OBJ_setEndMethod(..)
+ */
+void OBJ_setUpdateMethod(Object* obj, ObjectCallback* updateMethod);
+/**
+ *  \brief
+ *      Set the ending method for the given object
+ *
+ *  \param obj
+ *      Object to set <i>end</i> method for
+ *  \param initMethod
+ *      the method to set for object destruction (should be called just before object is released)
+ *
+ *  \see OBJ_setInitMethod(..)
+ *  \see OBJ_setUpdateMethod(..)
+ */
+void OBJ_setEndMethod(Object* obj, ObjectCallback* endMethod);
+
+
+
+#endif // _OBJECT_H_
---
+++ src/object.c
@@ -0,0 +1,90 @@
+#include "config.h"
+#include "types.h"
+
+#include "pool.h"
+
+#include "object.h"
+
+#include "memory.h"
+#include "kdebug.h"
+
+
+static void dummyObjectMethod(Object* obj);
+
+
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+static bool isAllocated(Object* obj)
+{
+    return obj->state & OBJ_ALLOCATED;
+}
+#endif
+
+static bool checkValid(Object* obj, char* methodName)
+{
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+    if (!isAllocated(obj))
+    {
+        kprintf("%s: object %p is not allocated !", methodName, obj);
+        return FALSE;
+    }
+#endif
+
+    return TRUE;
+}
+
+
+Pool* OBJ_createObjectPool(u16 size, u16 objectSize)
+{
+    return POOL_create(size, objectSize);
+}
+
+
+Object* OBJ_create(Pool* pool)
+{
+    Object* result = POOL_allocateObject(pool);
+
+    if (result == NULL) return NULL;
+
+    // object is allocated
+    result->state = OBJ_ALLOCATED;
+
+    return result;
+}
+
+void OBJ_release(Pool* pool, Object* obj)
+{
+    if (!checkValid(obj, "OBJ_release")) return;
+
+    POOL_releaseObject(pool, obj);
+}
+
+
+void OBJ_setInitMethod(Object* obj, ObjectCallback* initMethod)
+{
+    if (!checkValid(obj, "OBJ_setInitMethod")) return;
+
+    if (initMethod != NULL) obj->init = initMethod;
+    else obj->init = dummyObjectMethod;
+}
+
+void OBJ_setUpdateMethod(Object* obj, ObjectCallback* updateMethod)
+{
+    if (!checkValid(obj, "OBJ_setUpdateMethod")) return;
+
+    if (updateMethod != NULL) obj->update = updateMethod;
+    else obj->update = dummyObjectMethod;
+}
+
+void OBJ_setEndMethod(Object* obj, ObjectCallback* endMethod)
+{
+    if (!checkValid(obj, "OBJ_setEndMethod")) return;
+
+    if (endMethod != NULL) obj->end = endMethod;
+    else obj->end = dummyObjectMethod;
+}
+
+
+static void dummyObjectMethod(Object* obj)
+{
+    //
+}
