7eca14cc5712f9488b232c7bc458fbc8f7e2f422
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2022-03-03 17:29:22 +0100

    Added new pool object allocator unit (pool.h)

---
+++ inc/pool.h
@@ -0,0 +1,145 @@
+/**
+ *  \file pool.h
+ *  \brief Pool object management unit.
+ *  \author Stephane Dallongeville
+ *  \date 02/2022
+ *
+ * This unit provides methods to manage dynamic object allocation.<br>
+ * <br>
+ * You can use <i>Pool</i> object to handle dynamic allocation from a fixed set of objects.<br>
+ * For instance if you may need to handle dynamically bullets for your game and you want to have
+ * at max 20 bullets, you can handle it that way:<pre>
+ * Pool* bulletPool POOL_create(20, sizeof(Bullet));
+ * ...
+ * // create a new bullet
+ * Bullet* bullet = POOL_allocateObject(bulletPool);
+ * // check if bullet was correctly created and do your stuff..
+ * if (bullet != NULL)
+ * {
+ *    ...
+ * }
+ * ...
+ * // release your bullet
+ * POOL_releaseObject(bulletPool, bullet);
+ * </pre>
+ * <i>Pool</i> object is also very useful for fast iteration over allocated objects:<pre>
+ * Bullet** bullets = bulletPool->allocStack;
+ * u16 num = POOL_getNumAllocated(bulletPool);
+ * while(num--)
+ * {
+ *    Bullet* bullet = *bullets++;
+ *    // do whatever you need on your bullet
+ *    ...
+ * }</pre>
+ *
+ */
+
+#ifndef _POOL_H_
+#define _POOL_H_
+
+
+/**
+ *  \brief
+ *      Object pool allocator structure
+ *
+ *  \param bank
+ *      bank data
+ *  \param allocStack
+ *      allocation stack used for fast allocate / release operation
+ *  \param ree      point on first available object in the allocation stack
+ *  \param objectSize
+ *      size of a single object (in bytes)
+ *  \param size
+ *      size of the object pool (in number of object)
+ */
+typedef struct
+{
+    void* bank;
+    void** allocStack;
+    void** free;
+    u16 objectSize;
+    u16 size;
+} Pool;
+
+
+/**
+ *  \brief
+ *      Create and allocate a new object pool allocator
+ *
+ *  \param size
+ *      the capacity of the pool (in number of object)
+ *  \param objectSize
+ *      the size of a single object (usually you should use sizeof(Struct) here)
+ *
+ *  \return the new created object pool or NULL if there is not enough memory available for that.
+ *
+ *  \see POOL_destroy(..)
+ *  \see POOL_reset(..)
+ */
+Pool* POOL_create(u16 size, u16 objectSize);
+/**
+ *  \brief
+ *      Release the specified object pool allocator
+ *
+ *  \param pool
+ *      Object pool allocator to release
+ *
+ *  \see POOL_create(..)
+ */
+void POOL_destroy(Pool* pool);
+
+/**
+ *  \brief
+ *      Reset the object pool allocator
+ *
+ *  \param pool
+ *      Object pool allocator to reset
+ *  \param clear
+ *      if set to TRUE then objects memory is cleared (initialized to 0)
+ */
+void POOL_reset(Pool* pool, bool clear);
+
+/**
+ *  \brief
+ *      Allocate a new objet from the specified object pool
+ *
+ *  \param pool
+ *      Object pool allocator
+ *
+ *  \return the allocated object or NULL if an error occured (no more available object in pool or invalid pool)
+ *
+ *  \see POOL_releaseObject(..)
+ */
+void* POOL_allocateObject(Pool* pool);
+/**
+ *  \brief
+ *      Release an objet from the specified object pool
+ *
+ *  \param pool
+ *      Object pool allocator
+ *  \param obj
+ *      Object to release
+ *
+ *  \see POOL_allocateObject(..)
+ */
+void POOL_releaseObject(Pool* pool, void* obj);
+
+/**
+ *  \return
+ *      the number of free object in the pool
+ *
+ *  \param pool
+ *      Object pool allocator
+ */
+u16 POOL_getFree(Pool* pool);
+/**
+ *  \return
+ *      the number of allocated object in the pool
+ *
+ *  \param pool
+ *      Object pool allocator
+ */
+u16 POOL_getNumAllocated(Pool* pool);
+
+
+#endif // _POOL_H_
---
+++ src/pool.c
@@ -0,0 +1,154 @@
+#include "config.h"
+#include "types.h"
+
+#include "pool.h"
+
+#include "memory.h"
+
+#include "kdebug.h"
+
+
+Pool* POOL_create(u16 size, u16 objectSize)
+{
+    // create the pool
+    Pool* result = MEM_alloc(sizeof(Pool));
+
+    // error on allocation --> return NULL
+    if (result == NULL) return NULL;
+
+    // allocate bank
+    result->bank = MEM_alloc(size * objectSize);
+    // can't allocate ? --> and return NULL
+    if (result->bank == NULL)
+    {
+        // release pool
+        MEM_free(result);
+
+        // and return NULL
+        return NULL;
+    }
+
+    // allocate stack
+    result->allocStack = MEM_alloc(size * sizeof(void*));
+    // can't allocate ?
+    if (result->allocStack == NULL)
+    {
+        // release bank & pool
+        MEM_free(result->bank);
+        MEM_free(result);
+
+        // and return NULL
+        return NULL;
+    }
+
+    // set size
+    result->size = size;
+    result->objectSize = objectSize;
+
+    POOL_reset(result, TRUE);
+
+    return result;
+}
+
+void POOL_reset(Pool* pool, bool clear)
+{
+    u16 i;
+    u16 os;
+    u8* d;
+    void** s;
+
+    // clear bank
+    if (clear)
+        memset(pool->bank, 0, pool->size * pool->objectSize);
+
+    // reset allocation stack
+    s = pool->allocStack;
+    d = pool->bank;
+    os = pool->objectSize;
+    i = pool->size;
+    while(i--)
+    {
+        // set alloc stack pointer
+        *s++ = d;
+        // point on next object
+        d += os;
+    }
+
+    // init 'first' position (point at the end of alloc stack)
+    pool->free = s;
+}
+
+void POOL_destroy(Pool* pool)
+{
+    if (pool)
+    {
+        if (pool->allocStack)
+        {
+            // release alloc stack / bank and the pool itself
+            MEM_free(pool->allocStack);
+            pool->allocStack = NULL;
+        }
+        if (pool->bank)
+        {
+            MEM_free(pool->bank);
+            pool->bank = NULL;
+        }
+
+        MEM_free(pool);
+    }
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+    else
+    {
+        kprintf("OBJ_destroyPool(): error - trying to destroy a not allocated pool !");
+    }
+#endif
+}
+
+
+void* POOL_allocateObject(Pool* pool)
+{
+    // pool is empty ?
+    if (pool->free == pool->allocStack)
+    {
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+        KLog("OBJ_allocateObject(): failed - no more available object !");
+#endif
+
+        return NULL;
+    }
+
+    // allocate object
+    return *--(pool->free);
+}
+
+void POOL_releaseObject(Pool* pool, void* obj)
+{
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+    if (obj == NULL)
+    {
+        KLog("OBJ_releaseObject(): failed - trying to release a NULL object !");
+        return;
+    }
+
+    // above
+    if (pool->free >= &pool->allocStack[pool->size])
+    {
+        KLog("OBJ_releaseObject(): failed - pool doesn't contain any object !");
+        return;
+    }
+#endif
+
+    // release object
+    *(pool->free)++ = obj;
+}
+
+
+u16 POOL_getFree(Pool* pool)
+{
+    return pool->free - pool->allocStack;
+}
+
+u16 POOL_getNumAllocated(Pool* pool)
+{
+    return pool->size - POOL_getFree(pool);
+}
