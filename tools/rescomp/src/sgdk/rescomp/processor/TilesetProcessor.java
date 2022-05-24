211004a11636c55fdab71bcd2cc942b1466e1d20
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2022-05-24 23:36:19 +0200

    Added RGB image + Tiled TSX file support

--- tools/rescomp/src/sgdk/rescomp/processor/TilesetProcessor.java
+++ tools/rescomp/src/sgdk/rescomp/processor/TilesetProcessor.java
@@ -1,7 +1,5 @@
 package sgdk.rescomp.processor;
 
-import java.io.IOException;
-
 import sgdk.rescomp.Compiler;
 import sgdk.rescomp.Processor;
 import sgdk.rescomp.Resource;
@@ -9,6 +7,7 @@ import sgdk.rescomp.resource.Tileset;
 import sgdk.rescomp.tool.Util;
 import sgdk.rescomp.type.Basics.Compression;
 import sgdk.rescomp.type.Basics.TileOptimization;
+import sgdk.rescomp.type.TSX;
 import sgdk.tool.FileUtil;
 
 public class TilesetProcessor implements Processor
@@ -20,22 +19,22 @@ public class TilesetProcessor implements Processor
     }
 
     @Override
-    public Resource execute(String[] fields) throws IllegalArgumentException, IOException
+    public Resource execute(String[] fields) throws Exception
     {
         if (fields.length < 3)
         {
             System.out.println("Wrong TILESET definition");
             System.out.println("TILESET name \"file\" [compression [opt]]");
             System.out.println("  name          Tileset variable name");
-            System.out.println("  file          the image to convert to TileSet structure (indexed colors BMP or PNG image)");
+            System.out.println("  file          path of the input file (BMP, PNG image file or TSX Tiled file)");
             System.out.println("  compression   compression type, accepted values:");
             System.out.println("                 -1 / BEST / AUTO = use best compression");
             System.out.println("                  0 / NONE        = no compression (default)");
             System.out.println("                  1 / APLIB       = aplib library (good compression ratio but slow)");
             System.out.println("                  2 / FAST / LZ4W = custom lz4 compression (average compression ratio but fast)");
             System.out.println("  opt           define the optimisation level, accepted values:");
-            System.out.println("                  0 / NONE        = no optimisation, each tile is unique");
-            System.out.println("                  1 / ALL         = ignore duplicated and flipped tile (default)");
+            System.out.println("                  0 / NONE        = no optimisation, each tile is unique (default for TSX file)");
+            System.out.println("                  1 / ALL         = ignore duplicated and flipped tile (default for image file)");
             System.out.println("                  2 / DUPLICATE   = ignore duplicated tile only");
 
             return null;
@@ -45,19 +44,23 @@ public class TilesetProcessor implements Processor
         final String id = fields[1];
         // get input file
         final String fileIn = FileUtil.adjustPath(Compiler.resDir, fields[2]);
+
+        // TSX file
+        final boolean tsx = FileUtil.getFileExtension(fileIn, false).toLowerCase().equals("tsx");
+
         // get packed value
         Compression compression = Compression.NONE;
         if (fields.length >= 4)
             compression = Util.getCompression(fields[3]);
         // get optimization value
         TileOptimization opt = TileOptimization.ALL;
-        if (fields.length >= 5)
+        // force ALL for TSX format
+        if (!tsx && (fields.length >= 5))
             opt = Util.getTileOpt(fields[4]);
 
         // add resource file (used for deps generation)
         Compiler.addResourceFile(fileIn);
 
-        // TILESET resource never optimize tiles and we ignore palette information
-        return Tileset.getTileset(id, fileIn, compression, opt, 256);
+        return Tileset.getTileset(id, tsx ? TSX.getTSXTilesetPath(fileIn) : fileIn, compression, opt, tsx);
     }
 }
---
+++ tools/rescomp/src/sgdk/rescomp/type/TSX.java
@@ -0,0 +1,181 @@
+package sgdk.rescomp.type;
+
+import java.io.FileNotFoundException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import sgdk.rescomp.resource.Tileset;
+import sgdk.rescomp.tool.Util;
+import sgdk.rescomp.type.Basics.Compression;
+import sgdk.rescomp.type.Basics.TileOptimization;
+import sgdk.tool.FileUtil;
+import sgdk.tool.StringUtil;
+import sgdk.tool.XMLUtil;
+
+public class TSX
+{
+    static final String ID_TILESET = "tileset";
+    static final String ID_IMAGE = "image";
+
+    static final String ATTR_VERSION = "version";
+    static final String ATTR_TILEDVERSION = "tiledversion";
+    static final String ATTR_NAME = "name";
+    static final String ATTR_TILEWIDTH = "tilewidth";
+    static final String ATTR_TILEHEIGHT = "tileheight";
+    static final String ATTR_WIDTH = "width";
+    static final String ATTR_HEIGHT = "height";
+    static final String ATTR_TILECOUNT = "tilecount";
+    static final String ATTR_COLUMNS = "columns";
+    static final String ATTR_SOURCE = "source";
+    static final String ATTR_TRANS = "trans";
+
+    // <?xml version="1.0" encoding="UTF-8"?>
+    // <tileset version="1.2" tiledversion="2019.02.10" name="Bathing_Spot" tilewidth="16" tileheight="16"
+    // tilecount="368" columns="23">
+    // <image source="../tileset/Bathing_Spot.png" trans="e00080" width="368" height="256"/>
+    // </tileset>
+
+    public static class TSXTileset implements Comparable<TSXTileset>
+    {
+        public final String file;
+        public final int startTileIndex;
+        public  int tileSize;
+        public int imageTileWidth;
+        public int imageTileHeigt;
+        public int numTile;
+        public int transparentColor;
+        public String tilesetImagePath;
+
+        public TSXTileset(String tmxFile, int startInd, Element tilesetElement) throws Exception
+        {
+            super();
+            
+            file = tmxFile;
+            startTileIndex = startInd;
+           
+            load(tilesetElement);
+        }
+
+        public TSXTileset(String tsxFile, int startInd) throws Exception
+        {
+            if (!FileUtil.exists(tsxFile))
+                throw new FileNotFoundException("TSX file '" + tsxFile + " not found !");
+            
+            file = tsxFile;
+            startTileIndex = startInd;
+
+            load(XMLUtil.getRootElement(XMLUtil.loadDocument(file)));
+        }
+        
+        private void load(Element tilesetElement) throws Exception
+        {
+            // check this is the tileset node
+            if (!tilesetElement.getNodeName().toLowerCase().equals(ID_TILESET))
+                throw new Exception("Expected " + ID_TILESET + " root node in TSX file: " + file + ", " + tilesetElement.getNodeName() + " found.");
+
+            final int tw = XMLUtil.getAttributeIntValue(tilesetElement, ATTR_TILEWIDTH, 0);
+            final int th = XMLUtil.getAttributeIntValue(tilesetElement, ATTR_TILEHEIGHT, 0);
+
+            if (tw != th)
+                throw new Exception("Non square tile not supported (" + tw + " x " + th + ") in TSX file: " + file);
+            if ((tw & 7) != 0)
+                throw new Exception("Unsuported tile size (should be a multiple of 8) in TSX file: " + file);
+            if ((tw < 8) || (tw > 32))
+                throw new Exception(tw + " x " + th + " tile size not supported (only 8x8 to 32x32 allowed) in TSX file: " + file);
+
+            tileSize = tw;
+
+            final int w = XMLUtil.getAttributeIntValue(tilesetElement, ATTR_COLUMNS, 0);
+            if (w == 0)
+                throw new Exception("Tileset 'columns' information not found in TSX file: " + file);
+
+            final Element imageElement = getElement(tilesetElement, ID_IMAGE, file);
+
+            if (XMLUtil.getAttributeIntValue(imageElement, ATTR_WIDTH, 0) != (w * tileSize))
+                throw new Exception("Image width (" + XMLUtil.getAttributeIntValue(imageElement, ATTR_WIDTH, 0) + ") do not match tileset size ("
+                        + (w * tileSize) + ") in TSX file: " + file);
+
+            imageTileWidth = w;
+            imageTileHeigt = XMLUtil.getAttributeIntValue(imageElement, ATTR_HEIGHT, 0) / tileSize;
+
+            // if ((imageTileWidth * imageTileHeigt) != XMLUtil.getAttributeIntValue(tilesetElement, ATTR_TILECOUNT, 0))
+            // System.out.print("Warning: Tileset 'tilecount' do not match 'image size' / 'tile size' in TSX file: " +
+            // file);
+
+            numTile = imageTileWidth * imageTileHeigt;
+
+            transparentColor = XMLUtil.getAttributeIntValue(imageElement, ATTR_TRANS, 0);
+            tilesetImagePath = XMLUtil.getAttributeValue(imageElement, ATTR_SOURCE, "");        }
+
+        public boolean containsTile(int tileInd)
+        {
+            return (tileInd >= startTileIndex) && (tileInd < (startTileIndex + numTile));
+        }
+
+        public String getTilesetPath() throws FileNotFoundException
+        {
+            return Util.getAdjustedPath(tilesetImagePath, file);
+        }
+
+        // return optimized tilset
+        public Tileset getTileset(String id, Compression compression) throws Exception
+        {
+            // always add a blank tile if not resent for TSX tileset (Tiled does not count it)
+            return Tileset.getTileset(id, getTilesetPath(), compression, TileOptimization.ALL, true);
+        }
+
+        public byte[] getTilesetImage8bpp(boolean cropPalette) throws Exception
+        {
+            final byte[] result = Util.getImage8bpp(getTilesetPath(), true, cropPalette);
+
+            // happen when we couldn't retrieve palette data from RGB image
+            if (result == null)
+                throw new IllegalArgumentException(
+                        "RGB image '" + getTilesetPath() + "' does not contains palette data (see 'Important note about image format' in the rescomp.txt file");
+
+            return result;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "tileSize=" + tileSize + " - startInd=" + startTileIndex + " - numTile=" + numTile;
+        }
+
+        @Override
+        public int compareTo(TSXTileset t)
+        {
+            return Integer.compare(startTileIndex, t.startTileIndex);
+        }
+    }
+
+    public static String getTSXTilesetPath(String file) throws Exception
+    {
+        return new TSXTileset(file, 0).getTilesetPath();
+    }
+
+    static Element getElement(Node node, String name, String file) throws Exception
+    {
+        final Element result = XMLUtil.getElement(node, name);
+
+        if (result == null)
+            throw new Exception("Cannot find " + name + " XML node in TSX file: " + file);
+
+        return result;
+    }
+
+    static String getAttribute(Element element, String attrName, String def)
+    {
+        return XMLUtil.getAttributeValue(element, attrName, def).toLowerCase();
+    }
+
+    static void checkAttributValue(Element element, String attrName, String value, String def, String file) throws Exception
+    {
+        final String attrValue = getAttribute(element, attrName, def);
+
+        if (!StringUtil.equals(attrValue, value))
+            throw new Exception("'" + attrValue + "' " + attrName + " not supported (" + def + " expected) in TSX file: " + file);
+    }
+}
