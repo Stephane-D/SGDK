8828a06d9b84ae1d18714882658cb58515b60fc9
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2022-06-07 23:46:48 +0200

    Added MEM_allocAt(..) function to allocate memory at a fixed address

--- src/memory.c
+++ src/memory.c
@@ -148,6 +148,7 @@ extern u32 _bend;
 
  // forward
 static u16* pack(u16 nsize);
+static u16* packEx(u32 addr, u16 nsize);
 
 static u16* free;
 static u16* heap;
@@ -168,7 +169,7 @@ void MEM_init()
     len = MEMORY_HIGH - (h + sizeof(u16));
 
 #if (MODULE_FRACTAL != 0)
-    // reserve fixed block of memory for fratal sound driver in high memory area
+    // reserve fixed block of memory for Fractal sound driver in high memory area
     len -= sizeof(Fractal_Data);
     // align on word
     len &= 0xFFFE;
@@ -322,6 +323,68 @@ void* MEM_alloc(u16 size)
 
 #if (LIB_LOG_LEVEL >= LOG_LEVEL_INFO)
     kprintf("MEM_alloc(%d) success: %lx - remaining = %d", size, (u32) p, MEM_getFree());
+#endif
+
+    // return block
+    return p;
+}
+
+void* MEM_allocAt(u32 addr, u16 size)
+{
+    u16* p;
+    u16 adjsize;
+    u16 remaining;
+
+    if (size == 0)
+        return 0;
+
+    // 2 bytes aligned
+    adjsize = (size + sizeof(u16) + 1) & 0xFFFE;
+
+    if (adjsize > *free)
+    {
+        p = packEx(addr, adjsize);
+
+        // block not available enough memory
+        if (p == NULL)
+        {
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_ERROR)
+            kprintf("MEM_allocEx(%8lx, %d) failed: wanted block is not available", addr, size);
+#endif
+
+            return NULL;
+        }
+
+        free = p;
+    }
+    else
+        // at this point we can allocate memory
+        p = free;
+
+    // set free to next free block
+    free += adjsize >> 1;
+
+    // get remaining (old - allocated)
+    remaining = *p - adjsize;
+    // adjust remaining free space
+    if (remaining > 0) *free = remaining;
+    else
+    {
+        // no more space in bloc so we have to find the next free bloc
+        u16 *newfree = free;
+        u16 bloc;
+
+        while((bloc = *newfree) & USED)
+            newfree += bloc >> 1;
+
+        free = newfree;
+    }
+
+    // set block size, mark as used and point to free region
+    *p++ = adjsize | USED;
+
+#if (LIB_LOG_LEVEL >= LOG_LEVEL_INFO)
+    kprintf("MEM_allocEx(%8lx, %d) success: %lx - remaining = %d", addr, size, (u32) p, MEM_getFree());
 #endif
 
     // return block
@@ -493,6 +556,83 @@ static u16* pack(u16 nsize)
                 // return it if greater than what we're looking for
                 if (bsize >= nsize)
                     return best;
+
+                // reset packed free size
+                bsize = 0;
+            }
+
+            // point to next memory block
+            b += psize >> 1;
+            // remember it in case it becomes free
+            best = b;
+        }
+        else
+        {
+            // increment free size
+            bsize += psize;
+            // clear this memory block as it will be packed
+            *b = 0;
+            // point to next memory block
+            b += psize >> 1;
+        }
+    }
+
+    // last free block update
+    if (bsize != 0)
+    {
+        // store packed free size
+        *best = bsize;
+
+        // return it if greater than what we're looking for
+        if (bsize >= nsize)
+            return best;
+    }
+
+    return NULL;
+}
+
+/*
+ * Pack free block and return first matching free block.
+ */
+static u16* packEx(u32 addr, u16 nsize)
+{
+    u16 *b;
+    u16 *best;
+    u16 bsize, psize;
+
+    b = heap;
+    best = b;
+    bsize = 0;
+
+    while ((psize = *b))
+    {
+        if (psize & USED)
+        {
+            if (bsize != 0)
+            {
+                // store packed free size
+                *best = bsize;
+
+                // greater than what we're looking for ?
+                if (bsize >= nsize)
+                {
+                    // get block address
+                    u32 baddr = (u32) best;
+
+                    // test if the block is where we want
+                    if ((baddr <= addr) && ((baddr + bsize) >= (addr + nsize)))
+                    {
+                        // free space before wanted block
+                        u16 delta = addr - baddr;
+
+                        // change available size for previous block
+                        if (baddr < addr)
+                            *best = delta;
+
+                        // that should match addr
+                        return best + (delta >> 1);
+                    }
+                }
 
                 // reset packed free size
                 bsize = 0;
