d7ea8f52dde3e5bddf266839b68feeb62504f374
Andreas Dietrich <43281521+a-dietrich@users.noreply.github.com>
2022-08-27 18:51:05 +0200

    Add TTY console support
    * Add console functions to inc/ext and src/ext
    * Add stb_sprintf.h to inc/ext/stb for optional use
    * Add console sample
    * Update doxyfile

---
+++ inc/ext/console.h
@@ -0,0 +1,392 @@
+
+/**
+ *  \file   console.h
+ *  \brief  TTY text console
+ *  \author Andreas Dietrich
+ *  \date   09/2022
+ *
+ *  This unit provides a simple TTY text console. Characters are written as a
+ *  stream, where lines are automatically wrapped if the horizontal border of
+ *  the screen is reached. When at the bottom of the screen window, the console
+ *  content is moved up by one text line and a blank row is inserted.
+ *
+ *  Per default, the console occupies a standad screen of 40x28 tiles. All text
+ *  attributes, such as font, palette, plane etc., are taken from SGDK text
+ *  settings. Screen updates are done using DMA transfer mode (which can be
+ *  changed with CON_setTransferMethod()).
+ *
+ *  One of the use cases are assert messages. To this end, the Genesis state can
+ *  be automatically reset before text is displayed (see assert macro below).
+ */
+
+#pragma once
+
+// *****************************************************************************
+//
+//  Includes
+//
+// *****************************************************************************
+
+// SGDK
+#include <genesis.h>
+
+// *****************************************************************************
+//
+//  Defines
+//
+// *****************************************************************************
+
+/// Comment out to disable asserts
+#define ENABLE_ASSERT_CHECKS
+
+// *****************************************************************************
+//
+//  Types
+//
+// *****************************************************************************
+
+typedef u32 size_t;
+typedef u32 ptrdiff_t;
+
+/// Callback prototype for a vsprintf() function using va_list
+typedef int (*vsprintf_t)(char *buf, const char *fmt, va_list args);
+/// Callback prototype for a vsnprintf() function using va_list
+typedef int (*vsnprintf_t)(char *buf, int count, const char *fmt, va_list args);
+
+// *****************************************************************************
+//
+//  Macros
+//
+// *****************************************************************************
+
+/// Helper macro to stringify numbers
+#define str(s) xstr(s)
+/// Helper macro
+#define xstr(s) #s
+
+/**
+ *  \brief
+ *      Assert a condition.
+ *
+ *  \param condition
+ *      Expression that must evaluate to TRUE, otherwise an error error message
+ *      in printed to the console. This will also reset the system to the SGDK
+ *      default state.
+ *
+ *  Can be disabled by undefining ENABLE_ASSERT_CHECKS or by defining NDEBUG.
+ */
+
+#if defined(ENABLE_ASSERT_CHECKS) && !defined(NDEBUG)
+#define assert(condition)                                                              \
+    if (! (bool)(u32)(condition) )                                                     \
+    {                                                                                  \
+        CON_reset();                                                                   \
+        CON_systemResetOnNextWrite();                                                  \
+        CON_write(__FILE__":"str(__LINE__)": Assertion \'"str(condition)"\' failed."); \
+        while (TRUE);                                                                  \
+    }
+#else
+#define assert(condition)
+#endif
+
+/**
+ *  \brief
+ *      Assert a condition.
+ *
+ * Uppercase version. Same as assert().
+ */
+
+#define ASSERT(condition) assert(condition)
+
+// *****************************************************************************
+//
+//  Function Declarations
+//
+// *****************************************************************************
+
+// -----------------------------------------------------------------------------
+// printf functions
+// -----------------------------------------------------------------------------
+
+/**
+ *  \brief
+ *      Standard C library sprintf function.
+ *
+ *  \param buf
+ *      Pointer to the buffer where the resulting C-string is stored.
+ *  \param fmt
+ *      C-string that contains the text to be written. It can optionally
+ *      contain embedded format specifiers that are replaced by the values
+ *      specified in subsequent additional arguments and formatted as requested.
+ *      (See https://en.wikipedia.org/wiki/Printf_format_string)
+ *  \param ... (additional arguments)
+ *      The function may expect a sequence of additional arguments, each
+ *      containing a value to be used to replace a format specifier in the
+ *      format string.
+ *  \return
+ *      The total number of characters written is returned. This count does not
+ *      include the additional null-character automatically appended at the end
+ *      of the string.
+ *
+ *  Composes a string that would result from using a standard C library printf
+ *  function. In case fmt includes format specifiers (beginning with %), the
+ *  additional arguments are formatted and inserted in the resulting string
+ *  replacing their respective specifiers.
+ *
+ *  This function is a wrapper for a vsprintf type function. Per default SGDK's
+ *  built-in vsprintf() is used. CON_setVsprintf() can be used to register
+ *  another implementation, e.g., stbsp_vsprintf().
+ */
+
+int CON_sprintf(char* buf, const char *fmt, ...)  __attribute__ ((format (printf, 2, 3)));
+
+/**
+ *  \brief
+ *      Standard C library snprintf function.
+ *
+ *  \param buf
+ *      Pointer to the buffer where the resulting C-string is stored.
+ *  \param count
+ *      Maximum number of bytes to be used in the buffer. The generated string
+ *      has a length of at most count-1, leaving space for the additional
+ *      terminating null-character.
+ *  \param fmt
+ *      C-string that contains the text to be written. It can optionally
+ *      contain embedded format specifiers that are replaced by the values
+ *      specified in subsequent additional arguments and formatted as requested.
+ *      (See https://en.wikipedia.org/wiki/Printf_format_string)
+ *  \param ... (additional arguments)
+ *      The function may expect a sequence of additional arguments, each
+ *      containing a value to be used to replace a format specifier in the
+ *      format string.
+ *  \return
+ *      The total number of characters written is returned. This count does not
+ *      include the additional null-character automatically appended at the end
+ *      of the string.
+ *
+ *  Composes a string that would result from using a standard C library printf
+ *  function. In case fmt includes format specifiers (beginning with %), the
+ *  additional arguments are formatted and inserted in the resulting string
+ *  replacing their respective specifiers.
+ *
+ *  This function is a wrapper for a vsnprintf type function. Per default no
+ *  function is registered. CON_setVsnprintf() can be used to register an
+ *  implementation, e.g., stbsp_vsnprintf().
+ */
+
+int CON_snprintf(char* buf, int count, const char *fmt, ...)  __attribute__ ((format (printf, 3, 4)));
+
+/**
+ *  \brief
+ *      Register a standard vsprintf function as callback for CON_sprintf()
+ *
+ *  \param vsprintf_func
+ *      Function pointer to a standard C library vsprintf function
+ *
+ *  Registers a callback function that will be wrapped by CON_sprintf(). The
+ *  funcion supplied needs to be a vsprintf function that takes va_list as
+ *  arguments, defined as follows:
+ *
+ *  typedef int (*vsprintf_t)(char *buf, const char *fmt, va_list args);
+ *
+ *  Possible implementations are SGDK's vsprintf() or alternatively
+ *  stbsp_vsprintf() (http://github.com/nothings/stb).
+ */
+
+void CON_setVsprintf(vsprintf_t vsprintf_func);
+
+/**
+ *  \brief
+ *      Register a standard vsnprintf function as callback for CON_snprintf()
+ *
+ *  \param vsnprintf_func
+ *      Function pointer to a standard C library vsnprintf function
+ *
+ *  Registers a callback function that will be wrapped by CON_snprintf(). The
+ *  funcion supplied needs to be a vsnprintf function that takes va_list as
+ *  arguments, defined as follows:
+ *
+ *  typedef int (*vsnprintf_t)(char *buf, int count, const char *fmt, va_list args);
+ *
+ *  A possible implementation is stbsp_vsnprintf()
+ *  (http://github.com/nothings/stb).
+ */
+
+void CON_setVsnprintf(vsnprintf_t vsnprintf_func);
+
+// -----------------------------------------------------------------------------
+// Console setup
+// -----------------------------------------------------------------------------
+
+/**
+ *  \brief
+ *      Set the size of the console window.
+ *
+ *  \param left
+ *      Position of the leftmost window column in tiles. Default is 0.
+ *  \param top
+ *      Position of the topmost window row in tiles. Default is 0.
+ *  \param width
+ *      Width of the console window in tiles. Default is 40.
+ *  \param height
+ *      Height of the console window in tiles. Default is 28.
+ *
+ *  If either width or height are 0 then VDP_getScreenWidth() and
+ *  VDP_getScreenHeight() will be used to determine suitable default values.
+ */
+
+void CON_setConsoleSize(u16 left, u16 top, u16 width, u16 height);
+
+/**
+ *  \brief
+ *      Set the size of the character line buffer.
+ *
+ *  \param size
+ *      Buffer size in bytes. This is the number of characters the buffer can
+ *      hold including a terminating null-character.
+ *
+ *  Sets the size of the character buffer that is used internally to compose
+ *  the formatted output string which is printed on the console.
+ *
+ *  Upon calling CON_setLineBufferSize(), the previous line buffer memory is
+ *  immedieately freed. The new buffer will be allocated once the next call to
+ *  CON_write() occurs.
+ *
+ *  Per default, the buffer size is set to 160 characters. Note that if only a
+ *  vsprintf function is registered (see CON_setVsprintf()), the user is
+ *  responsible for making sure a call to CON_write() does not exceed the line
+ *  buffer. In case a vsnprintf function is registered (see CON_setVsnprintf()),
+ *  it will automtically check the buffer size and prevent overflows.
+ */
+
+void CON_setLineBufferSize(u16 size);
+
+/**
+ *  \brief
+ *      Set the transfer method used to upload the console tile buffer to VDP
+ *      RAM.
+ *
+ *  \param tm
+ *      Transfer method. <br>
+ *      Accepted values are: <br>
+ *      - CPU <br>
+ *      - DMA <br>
+ *      - DMA_QUEUE <br>
+ *      - DMA_QUEUE_COPY
+ *
+ *  This sets the transfer method used with VDP_setTileMapDataRect(). The
+ *  default value is DMA, which will cause an immediate upload. Note that when
+ *  DMA_QUEUE or DMA_QUEUE_COPY is used, the user is responsible for triggering
+ *  DMA upload, e.g., by calling SYS_doVBlankProcess().
+ */
+
+void CON_setTransferMethod(TransferMethod tm);
+
+/**
+ *  \brief
+ *      Reset the console.
+ *
+ *  This function is a shortcut for:
+ *
+ *  CON_setConsoleSize(0, 0, 40, 28); <br>
+ *  CON_setLineBufferSize(160); <br>
+ *  CON_setTransferMethod(DMA);
+ */
+
+void CON_reset();
+
+/**
+ *  \brief
+ *      Reset the system to the SGDK default state when CON_write() is called
+ *      next time.
+ *
+ *  To reset the system the following SGDK functions will be called:
+ *
+ *  - SYS_disableInts() <br>
+ *  - YM2612_reset() <br>
+ *  - PSG_init() <br>
+ *  - Z80_init() <br>
+ *  - VDP_init()
+ */
+
+void CON_systemResetOnNextWrite();
+
+// -----------------------------------------------------------------------------
+// Console write functions
+// -----------------------------------------------------------------------------
+
+/**
+ *  \brief
+ *      Clear the console window.
+ *
+ *  This clears the console window by filling it with space characters. It is
+ *  assumed that the first tile of the font is a space character. DMA settings
+ *  apply here as well.
+ */
+
+void CON_clear();
+
+/**
+ *  \brief
+ *      Set a new cursor position.
+ *
+ *  \param x
+ *      New cursor x position.
+ *  \param y
+ *      New cursor y position.
+ *
+ *  This function specifies a new column and row for the cursor. This is the
+ *  position where the next character will appear when CON_write() is processed.
+ *  A position of (0,0) relates to the tile in the top/left corner.
+ */
+
+void CON_setCursorPosition(u16 x, u16 y);
+
+/**
+ *  \brief
+ *      Return the current cursor position.
+ *
+ *  \return
+ *      A 2D vector containing the current (x,y) position of the cursor, where
+ *      (0,0) means the top/left corner of the console window.
+ */
+
+V2u16 CON_getCursorPosition();
+
+/**
+ *  \brief
+ *      Write a C-string to the console window.
+ *
+ *  \param fmt
+ *      C-string that contains the text to be written. It can optionally
+ *      contain embedded format specifiers that are replaced by the values
+ *      specified in subsequent additional arguments and formatted as requested.
+ *      (See https://en.wikipedia.org/wiki/Printf_format_string)
+ *  \param ... (additional arguments)
+ *      The function may expect a sequence of additional arguments, each
+ *      containing a value to be used to replace a format specifier in the
+ *      format string.
+ *  \return
+ *      The total number of characters written is returned. This count does not
+ *      include the additional null-character automatically appended at the end
+ *      of the string.
+ *
+ *  This function writes a C-string into the console window where lines are
+ *  automatically wrapped if the horizontal border of the screen is reached.
+ *  When at the bottom of the screen window, the console content is moved up by
+ *  one text line and a blank row is inserted.
+ *
+ *  Internally it uses either vsprintf or vsnprintf functions that have been
+ *  registered with CON_setVsprintf() or CON_setVsnprintf(), respectively.
+ *
+ *  While processing the string, this function evaluates and executes escape
+ *  control sequences. The following control characters are supported:
+ *
+ *  - \\b : backspace <br>
+ *  - \\n : new line (line feed) <br>
+ *  - \\r : carriage return <br>
+ *  - \\t : horizontal tab <br>
+ *  - \\v : vertical tab
+ */
+
+int CON_write(const char *fmt, ...)  __attribute__ ((format (printf, 1, 2)));
---
+++ src/ext/console.c
@@ -0,0 +1,475 @@
+
+/**
+ *  \file   console.c
+ *  \brief  TTY text console
+ *  \author Andreas Dietrich
+ *  \date   09/2022
+ *
+ *  This unit provides a simple TTY text console. Characters are written as a
+ *  stream, where lines are automatically wrapped if the horizontal border of
+ *  the screen is reached. When at the bottom of the screen window, the console
+ *  content is moved up by one text line and a blank row is inserted.
+ *
+ *  Per default, the console occupies a standad screen of 40x28 tiles. All text
+ *  attributes, such as font, palette, plane etc., are taken from SGDK text
+ *  settings. Screen updates are done using DMA transfer mode (which can be
+ *  changed with CON_setTransferMethod()).
+ *
+ *  One of the use cases are assert messages. To this end, the Genesis state can
+ *  be automatically reset before text is displayed (see assert macro below).
+ */
+
+// *****************************************************************************
+//
+//  Includes
+//
+// *****************************************************************************
+
+// Console
+#include "ext/console.h"
+
+// *****************************************************************************
+//
+//  External functions
+//
+// *****************************************************************************
+
+// SGDK vsprintf() used as default
+extern u16 vsprintf(char *buf, const char *fmt, va_list args);
+
+// *****************************************************************************
+//
+//  Defines
+//
+// *****************************************************************************
+
+#define CONSOLE_TAB_SIZE 8 // horizontal tab width
+
+// *****************************************************************************
+//
+//  Variables
+//
+// *****************************************************************************
+
+static bool m_consoleDoBufferReset = TRUE;
+static bool m_consoleDoSystemReset = FALSE;
+
+static u16 m_consoleX      =  0;
+static u16 m_consoleY      =  0;
+static u16 m_consoleLeft   =  0;
+static u16 m_consoleTop    =  0;
+static u16 m_consoleWidth  = 40;
+static u16 m_consoleHeight = 28;
+
+static TransferMethod m_consoleTransferMethod = DMA;
+
+static u16*  m_consoleFrameBuffer    = NULL;
+static char* m_consoleLineBuffer     = NULL;
+static u16   m_consoleLineBufferSize = 160;
+
+static vsprintf_t  m_vsprintf_func  = (vsprintf_t)vsprintf;
+static vsnprintf_t m_vsnprintf_func = NULL;
+
+// *****************************************************************************
+//
+//  Functions
+//
+// *****************************************************************************
+
+// -----------------------------------------------------------------------------
+//  Private console functions
+// -----------------------------------------------------------------------------
+
+static u16 consoleGetBasetile()
+{
+    // Build a basetile with current text attributes
+    return TILE_ATTR_FULL(
+        VDP_getTextPalette(),
+        VDP_getTextPriority(),
+        FALSE,
+        FALSE,
+        TILE_FONT_INDEX
+    );
+}
+
+// -----------------------------------------------------------------------------
+
+static u16* consoleGetFrameBuffer()
+{
+    // Reset SGDK state
+    if (m_consoleDoSystemReset)
+    {
+        SYS_disableInts();
+        YM2612_reset();
+        PSG_init();
+        Z80_init();
+        VDP_init();
+
+        m_consoleDoSystemReset = FALSE;
+    }
+
+    // Reset console buffer
+    if (m_consoleDoBufferReset)
+    {
+        const u16 basetile = consoleGetBasetile();
+        const u16 tiles    = m_consoleWidth * m_consoleHeight;
+        const u16 bytes    = tiles * 2;
+
+        // (Re)allocate frame buffer memory
+        MEM_free(m_consoleFrameBuffer);
+        m_consoleFrameBuffer = (u16*)MEM_alloc(bytes);
+
+        // Clear frame buffer memory
+        if (m_consoleFrameBuffer)
+        {
+            memsetU16(m_consoleFrameBuffer, basetile, tiles);
+            m_consoleDoBufferReset = FALSE;
+        }
+    }
+
+    return m_consoleFrameBuffer;
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleClearFrameBuffer()
+{
+    // Clear frame buffer memory
+    if (m_consoleFrameBuffer)
+    {
+        const u16 basetile = consoleGetBasetile();
+        const u16 tiles    = m_consoleWidth * m_consoleHeight;
+
+        memsetU16(m_consoleFrameBuffer, basetile, tiles);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+static char* consoleGetLineBuffer()
+{
+    // Allocate line buffer memory
+    if (!m_consoleLineBuffer)
+        m_consoleLineBuffer = MEM_alloc(m_consoleLineBufferSize);
+
+    return m_consoleLineBuffer;
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleScroll()
+{
+    // Scroll frame buffer
+    if (m_consoleFrameBuffer)
+    {
+        u16* const dst = m_consoleFrameBuffer;
+        u16* const src = dst + m_consoleWidth;
+
+        const u16 tiles    = m_consoleWidth * (m_consoleHeight-1);
+        const u16 basetile = consoleGetBasetile();
+
+        // Move upper part of buffer and clear last line
+        memcpyU16(dst, src, tiles);
+        memsetU16(dst+tiles, basetile, m_consoleWidth);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleUploadFrameBuffer()
+{
+    // Upload the console tile map to VDP RAM
+    if (m_consoleFrameBuffer)
+    {
+        VDP_setTileMapDataRect(
+            VDP_getTextPlane(),
+            m_consoleFrameBuffer,
+            m_consoleLeft,
+            m_consoleTop,
+            m_consoleWidth,
+            m_consoleHeight,
+            m_consoleWidth,
+            m_consoleTransferMethod
+        );
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleBackspace()
+{
+    // Mover cursor position one tile to the left
+    if (m_consoleX > 0)
+        m_consoleX--;
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleCarriageReturn()
+{
+    // Mover cursor position to start of current line
+    m_consoleX = 0;
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleHorizontalTab()
+{
+    // Move cursor to next tab positon.
+    // This is the next position divisible by CONSOLE_TAB_SIZE.
+    m_consoleX = min((m_consoleX/CONSOLE_TAB_SIZE + 1) * CONSOLE_TAB_SIZE,
+                      m_consoleWidth-1);
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleVerticalTab()
+{
+    // Move cursor down and scroll if we are past the bottom line
+    if (++m_consoleY >= m_consoleHeight)
+    {
+        m_consoleY = m_consoleHeight - 1;
+        consoleScroll();
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleNewLine()
+{
+    // Move cursor to the start of the next line
+    consoleCarriageReturn();
+    consoleVerticalTab();
+}
+
+// -----------------------------------------------------------------------------
+
+static void consoleCharacter(char c, u16* buffer, u16 basetile)
+{
+    // Test if we are past the right border and create a newline if so
+    if (m_consoleX >= m_consoleWidth)
+        consoleNewLine();
+
+    // Clamp cursor position to window
+    m_consoleX = min(m_consoleX, m_consoleWidth-1);
+    m_consoleY = min(m_consoleY, m_consoleHeight-1);
+
+    // Insert tile code (basetile index = ASCII code - 32)
+    buffer[m_consoleY * m_consoleWidth + m_consoleX] = basetile + (c - 32);
+
+    // Move cursor to the right. Note that we do not automatically create a new
+    // line if we are past the right border. This is because the next character
+    // might be '\n'. This avoids creating an unwanted blank row.
+    m_consoleX++;
+}
+
+// -----------------------------------------------------------------------------
+
+static void consolePrint(const char *str)
+{
+    // Get frame buffer pointer
+    u16* const buffer = consoleGetFrameBuffer();
+    if (!buffer)
+        return;
+
+    // Get base tile
+    const u16 basetile = consoleGetBasetile();
+
+    // Execute control code or insert new character
+    char c;
+    while ((c = *str++))
+    {
+        switch (c)
+        {
+            case '\b': consoleBackspace();      break;
+            case '\n': consoleNewLine();        break;
+            case '\r': consoleCarriageReturn(); break;
+            case '\t': consoleHorizontalTab();  break;
+            case '\v': consoleVerticalTab();    break;
+
+            default:
+                consoleCharacter(c, buffer, basetile);
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+//  Public printf functions
+// -----------------------------------------------------------------------------
+
+int CON_sprintf(char* buf, const char *fmt, ...)
+{
+    int len = 0;
+
+    // Collect arguments and call vsprintf
+    va_list args;
+    va_start(args, fmt);
+
+    if (m_vsprintf_func)
+        len = m_vsprintf_func(buf, fmt, args);
+
+    va_end(args);
+
+    return len;
+}
+
+// -----------------------------------------------------------------------------
+
+int CON_snprintf(char* buf, int count, const char *fmt, ...)
+{
+    int len = 0;
+
+    // Collect arguments and call vsnprintf
+    va_list args;
+    va_start(args, fmt);
+
+    if (m_vsnprintf_func)
+        len = m_vsnprintf_func(buf, count, fmt, args);
+
+    va_end(args);
+
+    return len;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_setVsprintf(vsprintf_t vsprintf_func)
+{
+    m_vsprintf_func = vsprintf_func;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_setVsnprintf(vsnprintf_t vsnprintf_func)
+{
+    m_vsnprintf_func = vsnprintf_func;
+}
+
+// -----------------------------------------------------------------------------
+//  Public console functions
+// -----------------------------------------------------------------------------
+
+void CON_setConsoleSize(u16 left, u16 top, u16 width, u16 height)
+{
+    // Set console window size (use system defaults if width/height is 0)
+    if (width && height)
+    {
+        m_consoleLeft   = left;
+        m_consoleTop    = top;
+        m_consoleWidth  = width;
+        m_consoleHeight = height;
+    }
+    else
+    {
+        m_consoleLeft   = 0;
+        m_consoleTop    = 0;
+        m_consoleWidth  = VDP_getScreenWidth() / 8;
+        m_consoleHeight = VDP_getScreenHeight() / 8;
+    }
+
+    // Reset cursor position
+    m_consoleX = m_consoleY = 0;
+
+    m_consoleDoBufferReset = TRUE;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_setLineBufferSize(u16 size)
+{
+    // Free current line buffer
+    MEM_free(m_consoleLineBuffer);
+    m_consoleLineBuffer = NULL;
+
+    // Only set new size, allocation will be done on next write
+    m_consoleLineBufferSize = size;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_setTransferMethod(TransferMethod tm)
+{
+    // SGDK transfer method used when uploading the buffer tile map
+    m_consoleTransferMethod = tm;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_reset()
+{
+    // Reset window size, line buffer size, and transfer method to defaults
+    CON_setConsoleSize(0, 0, 40, 28);
+    CON_setLineBufferSize(160);
+    CON_setTransferMethod(DMA);
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_systemResetOnNextWrite()
+{
+    // Reset Genesis to SGDK default state on next write
+    m_consoleDoSystemReset = TRUE;
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_clear()
+{
+    // Reset cursor position
+    m_consoleX = m_consoleY = 0;
+
+    // Clear and upload tile map
+    consoleClearFrameBuffer();
+    consoleUploadFrameBuffer();
+}
+
+// -----------------------------------------------------------------------------
+
+void CON_setCursorPosition(u16 x, u16 y)
+{
+    // Set new cursor position and clamp to window
+    m_consoleX = min(x, m_consoleWidth-1);
+    m_consoleY = min(y, m_consoleHeight-1);
+}
+
+// -----------------------------------------------------------------------------
+
+V2u16 CON_getCursorPosition()
+{
+    // Create vector containing cursor position
+    V2u16 result = { m_consoleX, m_consoleY };
+
+    return result;
+}
+
+// -----------------------------------------------------------------------------
+
+int CON_write(const char *fmt, ...)
+{
+    int   len = 0;
+    char* buf = consoleGetLineBuffer();
+
+    if (buf)
+    {
+        // Collect arguments
+        va_list args;
+        va_start(args, fmt);
+
+        // Format output string, prefer a function with buffer size check
+        if (m_vsnprintf_func)
+            len = m_vsnprintf_func(buf, m_consoleLineBufferSize, fmt, args);
+        else if (m_vsprintf_func)
+            len = m_vsprintf_func(buf, fmt, args);
+
+        va_end(args);
+
+        // Process characters and upload tile map
+        if (len)
+        {
+            consolePrint(buf);
+            consoleUploadFrameBuffer();
+        }
+    }
+
+    return len;
+}
