202bca1b9b2e5438c347bbd91475a626953ddb23
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2022-11-04 17:07:23 +0100

    updated version

--- tools/rescomp/src/sgdk/rescomp/Launcher.java
+++ tools/rescomp/src/sgdk/rescomp/Launcher.java
@@ -30,7 +30,7 @@ public class Launcher
                 depTarget = param;
         }
 
-        System.out.println("ResComp 3.61 - SGDK Resource Compiler - Copyright 2022 (Stephane Dallongeville)");
+        System.out.println("ResComp 3.62 - SGDK Resource Compiler - Copyright 2022 (Stephane Dallongeville)");
 
         if (fileName == null)
         {
