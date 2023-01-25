e806149859232f2d90521c5d8cf6a7a2d14694e0
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2023-01-26 00:15:58 +0100

    updated

--- src/z80_drv1.h
+++ src/z80_drv1.h
@@ -1,6 +1,6 @@
 #ifndef _Z80_DRV1_H_
 #define _Z80_DRV1_H_
 
-extern const u8 z80_drv1[0xAE5];
+extern const u8 z80_drv1[0xB2F];
 
 #endif // _Z80_DRV1_H_
--- src/z80_drv3.h
+++ src/z80_drv3.h
@@ -1,6 +1,6 @@
 #ifndef _Z80_DRV3_H_
 #define _Z80_DRV3_H_
 
-extern const u8 z80_drv3[0xA6C];
+extern const u8 z80_drv3[0xA6D];
 
 #endif // _Z80_DRV3_H_
