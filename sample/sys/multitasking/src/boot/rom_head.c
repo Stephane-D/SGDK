df94bd9be9e696402e86507761dcb60b7997d061
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2024-01-29 23:07:52 +0100

    moved 'console' and 'multitasking' samples into 'sample/sys' folder

---
+++ sample/sys/multitasking/src/boot/rom_head.c
@@ -0,0 +1,33 @@
+#include "genesis.h"
+
+__attribute__((externally_visible))
+const ROMHeader rom_header = {
+#if (ENABLE_BANK_SWITCH != 0)
+    "SEGA SSF        ",
+#elif (MODULE_MEGAWIFI != 0)
+    "SEGA MEGAWIFI   ",
+#else
+    "SEGA MEGA DRIVE ",
+#endif
+    "(C)SGDK 2023    ",
+    "SAMPLE PROGRAM                                  ",
+    "SAMPLE PROGRAM                                  ",
+    "GM 00000000-00",
+    0x000,
+    "JD              ",
+    0x00000000,
+#if (ENABLE_BANK_SWITCH != 0)
+    0x003FFFFF,
+#else
+    0x000FFFFF,
+#endif
+    0xE0FF0000,
+    0xE0FFFFFF,
+    "RA",
+    0xF820,
+    0x00200000,
+    0x0020FFFF,
+    "            ",
+    "DEMONSTRATION PROGRAM                   ",
+    "JUE             "
+};
