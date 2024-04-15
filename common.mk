551c0465bc042d42f43fc3914a154c96d7a71fc1
Craig Hackney <iratahack@digitalxfer.com>
2024-04-14 22:15:49 -0700

    Easier support for cross compiled GCC

--- common.mk
+++ common.mk
@@ -9,42 +9,25 @@ INCLUDE_LIB := $(GDK)/inc
 MAKEFILE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 MAKEFILE_DIR := $(subst \,/,$(MAKEFILE_DIR))
 
-ifeq ($(OS),Windows_NT)
-	# Native Windows
-	SHELL := $(BIN)/sh.exe
-	RM := $(BIN)/rm.exe
-	CP := $(BIN)/cp.exe
-	MKDIR := $(BIN)/mkdir.exe
-
-	AR := $(BIN)/ar.exe
-	CC := $(BIN)/gcc.exe
-	LD:= $(BIN)/ld.exe
-	NM:= $(BIN)/nm.exe
-	OBJCPY := $(BIN)/objcopy.exe
-	ASMZ80 := $(BIN)/sjasm.exe
-	MACCER := $(BIN)/mac68k.exe
-	BINTOS := $(BIN)/bintos.exe
-	LTO_PLUGIN := --plugin=liblto_plugin-0.dll
-	LIBGCC := $(LIB)/libgcc.a
-else
-	# Native Linux and Docker
+ifneq ($(OS),Windows_NT)
 	PREFIX ?= m68k-elf-
-	SHELL = sh
-	RM = rm
-	CP = cp
-	MKDIR = mkdir
-
-	AR := $(PREFIX)ar
-	CC := $(PREFIX)gcc
-	LD := $(PREFIX)ld
-	NM := $(PREFIX)nm
-	OBJCPY := $(PREFIX)objcopy
-	ASMZ80 := sjasm
-	MACCER := mac68k
-	BINTOS := bintos
-	LTO_PLUGIN :=
-	LIBGCC := -lgcc
 endif
+
+SHELL = sh
+RM = rm
+CP = cp
+MKDIR = mkdir
+
+AR := $(PREFIX)ar
+CC := $(PREFIX)gcc
+LD := $(PREFIX)ld
+NM := $(PREFIX)nm
+OBJCPY := $(PREFIX)objcopy
+ASMZ80 := sjasm
+MACCER := mac68k
+BINTOS := bintos
+LTO_PLUGIN :=
+LIBGCC := -lgcc
 
 JAVA := java
 ECHO := echo
