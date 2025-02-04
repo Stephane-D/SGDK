44bb5f393b747594b79a02d29791719b95a4b3b8
PaCHoN <juan@randombitsonfire.com>
2025-02-04 11:44:11 +0100

    f

--- sample/megawifi2/src/main.c
+++ sample/megawifi2/src/main.c
@@ -17,7 +17,6 @@
  * rebuild the library.
  * You can use: 
  * - MODULE_EVERDRIVE to 1 
- * - ENABLE_BANK_SWITCH to 1
  * in config.h to use everdive and MegaWifi Addon
  *
  * \author Juan Antonio (PaCHoN)
