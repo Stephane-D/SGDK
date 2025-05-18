323439f3f4b0bb3104ba22bc83162e7021098c3a
werton <weerbox@gmail.com>
2025-05-18 20:21:08 +0900

    removed comment

--- tools/rescomp/src/sgdk/rescomp/type/TMX.java
+++ tools/rescomp/src/sgdk/rescomp/type/TMX.java
@@ -10,7 +10,6 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-//import java.util.Objects;
 import java.util.Set;
 
 import org.w3c.dom.Document;
