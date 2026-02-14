266b9c09d34de7364e79ac64d06891bfea5049e1
juan <juan@randombitsonfire.com>
2026-02-14 20:21:49 +0100

    imprivement Vtable and comments and fix MEGAWIFI_IMPLEMENTATION_MW_CART on rom head

--- inc/ext/mw/comm.h
+++ inc/ext/mw/comm.h
@@ -1,5 +1,5 @@
 /************************************************************************
- * \brief COMM driver.
+ * \brief COMM driver Interface.
  *
  * \author Juan Antonio Ruiz (PaCHoN)
  * \date   2024-2025
@@ -13,14 +13,54 @@
 #include "types.h"
 /**************************************** */
 
+/**
+ * \brief Get the current receive buffer length.
+ * \return Number of bytes available to read in the receive buffer.
+ */
 u16 comm_get_buffer_length(void);
+
+/**
+ * \brief Get the current transmit FIFO length.
+ * \return Number of bytes currently in the transmit FIFO.
+ */
 u16 comm_get_tx_fifo_length(void);
 
+/**
+ * \brief Initialize the COMM communication interface.
+ * 
+ * Must be called before using any other COMM functions.
+ */
 void comm_init(void);
+
+/**
+ * \brief Write a single byte to the COMM interface.
+ * \param data The byte to transmit.
+ */
 void comm_write(u8 data);
+
+/**
+ * \brief Check if data is available to read.
+ * \return TRUE if data is available in the receive buffer, FALSE otherwise.
+ */
 bool comm_read_ready(void);
+
+/**
+ * \brief Check if the COMM interface is ready to accept data for transmission.
+ * \return TRUE if write buffer has space available, FALSE otherwise.
+ */
 bool comm_write_ready(void);
+
+/**
+ * \brief Read a single byte from the COMM interface.
+ * \return The byte read from the receive buffer.
+ * \note Call comm_read_ready() first to ensure data is available.
+ */
 u8 comm_read(void);
+
+/**
+ * \brief Get the current COMM operation mode.
+ * \return A string describing the current communication mode.
+ */
 char* comm_mode(void);
 #endif /*_COMM_H_*/
 
