45171f482d129330a2c2a3814d7083ef00135d1b
PaCHoN <juan@randombitsonfire.com>
2026-09-14 16:35:28 +0200

    mejora test e2e

--- project/template/tests/e2e/test_e2e_main.c
+++ project/template/tests/e2e/test_e2e_main.c
@@ -5,35 +5,22 @@ static u16 vblankProcessCalls;
 
 static bool testLoopShouldContinue(void)
 {
-    return vblankProcessCalls == 0;
+    return vblankProcessCalls <= 100;
 }
 
 static bool testSysDoVBlankProcess(void)
 {
     vblankProcessCalls++;
+    SYS_doVBlankProcess();
     return TRUE;
 }
 
 /* Replace hardware operations so the application main can run once on the host. */
 #define main applicationMain
 #define TRUE testLoopShouldContinue()
-#define SYS_disableInts() ((void)0)
-#define SYS_enableInts() ((void)0)
-#define SPR_init() ((void)0)
-#define JOY_setEventHandler(callback) ((void)0)
-#define XGM_startPlay(song) ((void)0)
-#define SPR_addSprite(...) ((void)0)
-#define SPR_update() ((void)0)
 #define SYS_doVBlankProcess() testSysDoVBlankProcess()
 #include "../../src/main.c"
 #undef SYS_doVBlankProcess
-#undef SPR_update
-#undef SPR_addSprite
-#undef XGM_startPlay
-#undef JOY_setEventHandler
-#undef SPR_init
-#undef SYS_enableInts
-#undef SYS_disableInts
 #undef TRUE
 #undef main
 
